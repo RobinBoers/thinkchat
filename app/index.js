89208345cb820897cb52b971c74ea77bf0ae90af
Robin Boers <60298132+RobinBoers@users.noreply.github.com>
2021-01-26 15:04:36 +0100

    Added favicon and SEO stuff

--- app/index.js
+++ app/index.js
@@ -134,3 +134,17 @@ socket.on('userCount', function (data) {
   console.log(data.userCount);
   document.getElementById("user-count").innerHTML = "<span class='count'>" + data.userCount + "</span> users online"
 });
+
+window.addEventListener('beforeinstallprompt', e => {
+  console.log('beforeinstallprompt Event fired');
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  this.deferredPrompt = e;
+  return false;
+  });
+// When you want to trigger prompt:
+this.deferredPrompt.prompt();
+  this.deferredPrompt.userChoice.then(choice => {
+  console.log(choice);
+  });
+this.deferredPrompt = null;
